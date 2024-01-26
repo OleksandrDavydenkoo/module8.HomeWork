3bf6ffc212c2af1adde7f26608f5a9444263aa63
Oleksandr <ol.davydenkoo@gmail.com>
2024-01-26 18:41:52 +0100

    HomeWork

--- src/main/java/org/example/Cylinder.java
+++ src/main/java/org/example/Cylinder.java
@@ -2,6 +2,6 @@ package org.example;
 public class Cylinder extends Shape {
     @Override
     public void displayShapeName() {
-        System.out.println("Cylinder");;
+        System.out.println("Cylinder");
     }
 }
